5c81db68a1a8ca59ee9743b97598794f0d5d791d
Azure <66933053+DualFennecFox@users.noreply.github.com>
2024-11-28 00:56:51 -0400

    cc

--- index.js
+++ index.js
@@ -1,105 +1,131 @@
 const Telegram = require('node-telegram-bot-api');
 require('dotenv-flow').config();
-const YT = require('scrape-yt')
-const ytdl = require("ytdl-core")
+const { Client } = require('youtubei')
+const ytdl = require("@distube/ytdl-core")
 const fs = require('fs');
-const client = new Telegram(process.env.TOKEN, {polling: true});
-let id 
-let name
-client.on('message', async msg => {
+const http = require('http');
+const https = require('https');
+const client = new Telegram(process.env.TOKEN, { polling: true });
+const shell = require('shelljs')
+const YT = new Client()
+let qindex = 0;
+async function downloadImage(url, title) {
 
-if (!msg.text) return
-
-if (msg.text.startsWith("/song")) {
-
-  YT.search(msg.text.replace('/eval', ''), { type: "video", limit: 10 }).then(async (videos) => {
-  if (videos.length < 1) return client.sendMessage(msg.chat.id, "No existe ningún resultado con ese nombre.").then(msg => setTimeout(() => client.deleteMessage(msg.chat.id, msg.message_id), 5000))
-
-  let vidNameArr = []
-  let videoID = []
-  let message = ''
-
-  for (let v = 0; v < videos.length; v++) {
-    videoID.push(`https://www.youtube.com/watch?v=${videos[v].id}`)
-    vidNameArr.push(`${v + 1}: ${videos[v].title}`);
-  
-}
-for (let v = 0; v < videos.length; v++) {
-
-  message += `${vidNameArr[v]}\n\n`
-}
-
-var menu = [0,2,4,6,8];
-
-  var options = {
-       reply_markup: JSON.stringify({
-            inline_keyboard: menu.map((x, xi) => ([{
-                text: String(x + 1),
-                callback_data: String(x + 1),
-                
-            }, { text: String(x + 2), callback_data: String(x + 2), 
-            }]
-            ))
+  return new Promise((resolve) => {
+    https.get(url, (res) => {
+      res.pipe(fs.createWriteStream(`./songs/${title}.jpg`));
+      res.on("end", () => {
+        resolve()
       })
-  };
+    });
 
-  client.sendMessage(msg.chat.id, `Elije la cancion que quieres escuchar según el numero\n\n${message}`, options)
-
-  id = videoID
-  name = videos
-})
+  })
 }
+
+let vidNameArr = []
+let videoArr = []
+let inlineArr = []
+client.on('message', async msg => {
+  vidNameArr = []
+  videoArr = []
+
+  if (!msg.text) return
+
+  if (msg.text.startsWith("/song")) {
+    let args = msg.text.replace('/song', '')
+    if (args == "") { return client.sendMessage(msg.chat.id, "Debes de escribir el nombre de una cancion.") }
+
+    YT.search(args, { type: "video" }).then(async (videos) => {
+      let length = videos.items.length
+      if (length < 1) return client.sendMessage(msg.chat.id, "No existe ningún resultado con ese nombre.").then(msg => setTimeout(() => client.deleteMessage(msg.chat.id, msg.message_id), 5000))
+      if (length > 10) length = 10
+      let message = ''
+
+      for (let v = 0; v < length; v++) {
+        videoArr = videos.items
+        vidNameArr.push(`${v + 1}: ${videos.items[v].title}`);
+        message += `${vidNameArr[v]}\n\n`
+
+      }
+
+      var menu = [0, 2, 4, 6, 8];
+
+      var options = {
+        reply_markup: JSON.stringify({
+          inline_keyboard: menu.map((x, xi) => ([{
+            text: String(x + 1),
+            callback_data: String(x + 1),
+
+          }, {
+            text: String(x + 2), callback_data: String(x + 2),
+          }]
+          ))
+        })
+      };
+
+      client.sendMessage(msg.chat.id, `Elije la cancion que quieres escuchar según el numero\n\n${message}`, options)
+
+    })
+  }
+})
+
+client.on("inline_query", (event) => {
+    console.log(event.query)
+
+    http.request({ host: "127.0.0.1", port: 3000, method: "GET", path: `?s=${event.query}`, headers: {
+      accept: "user/Agent"
+    }}, res => {
+      console.log(res)
+    })
 })
 
 client.on('callback_query', async q => {
 
-try {
+  try {
 
-  client.deleteMessage(q.message.chat.id, q.message.message_id)
-} catch (err) {
+    client.deleteMessage(q.message.chat.id, q.message.message_id)
+  } catch (err) {
 
-  console.error(err)
-}
-
-let video = name[q.data -1]
-
-  let audio = ytdl(id[q.data - 1], { filter: 'audioonly', quality: 'highestaudio' }).pipe(fs.createWriteStream(`${video.title}.mp3`))
-
+    console.error(err)
+  }
+  let video = videoArr[parseInt(q.data) - 1]
+  if (!video) return
+  let audio = ytdl(`https://www.youtube.com/watch?v=${video.id}`, { filter: 'audioonly', quality: 'highestaudio' }).pipe(fs.createWriteStream(`./songs/${video.title}.mp3`))
+  let thumbnail = video.thumbnails.min
+  await downloadImage(thumbnail, video.title)
 
   audio.on('finish', async () => {
 
-   await client.sendAudio(q.message.chat.id, `./${video.title}.mp3`, { duration: video.duration, title: video.title, 
-      thumb: video.thumbnail, performer: video.channel.name, })
 
-  fs.unlink(`${video.title}.mp3`, function (err) {
-  
-    console.log(err)
-    
-  })
+    await client.sendAudio(q.message.chat.id, `./songs/${video.title}.mp3`, {
+      duration: video.duration, title: video.title,
+      thumb: `./songs/${video.title}.jpg`, performer: video.channel?.name,
+    })
+
   })
 
 })
 
 client.onText(/\/eval (.+)/, async (msg, match) => {
 
-  if(msg.from.id != parseInt(process.env.OWNER)) return
+  if (msg.from.id != parseInt(process.env.OWNER)) return
   const chat = msg.chat.id;
 
   try {
-      const code = match[1]
-      let evaled = await eval(code)
-  
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-  
-      client.sendMessage(chat, clean(evaled), {code:"xl"});
-    } catch (err) {
-      client.sendMessage(chat, `Error\n\n${clean(err)}\n`);
-    }
+    const code = match[1]
+    let evaled = await eval(code)
+
+    if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+
+    client.sendMessage(chat, clean(evaled), { code: "xl" });
+  } catch (err) {
+    client.sendMessage(chat, `Error\n\n${clean(err)}\n`);
+  }
 })
 const clean = text => {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+  if (typeof (text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+    return text;
 }
